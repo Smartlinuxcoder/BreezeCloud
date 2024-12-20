fd620f1d21d70739859583acd4b9ed754bb359f5
Smartlinuxcoder <smartcoder@smartlinux.xyz>
2024-12-20 19:44:41 +0100

    minio test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Elysia } from 'elysia'
+/* import { Elysia } from 'elysia'
 import { staticPlugin } from '@elysiajs/static'
 import { auth } from './src/api/auth.js'
 
@@ -7,7 +7,7 @@ import { auth } from './src/api/auth.js'
 if (!process.env.JWT_SECRET){
     console.error('JWT_SECRET is not set');
 }
-const port = process.env.PORT || 3000
+const port = process.env.SERVER_PORT || 3000
 new Elysia()
     .use(auth)
     .use(staticPlugin({
@@ -18,4 +18,24 @@ new Elysia()
     .listen(port)
 
 
-console.log(`Listening on port ${port}`)
+console.log(`Listening on port ${port}`) */
+
+const Minio = require('minio');
+
+const minioClient = new Minio.Client({
+  endPoint: process.env.MINIO_HOSTNAME,
+  port: process.env.MINIO_PORT,                
+  useSSL: process.env.MINIO_SSL==='true', 
+  accessKey: process.env.MINIO_ACCESSKEY,
+  secretKey: process.env.MINIO_SECRETKEY
+});
+
+minioClient.fPutObject('cloud', 'myfile.txt', './myfile.txt', (err, etag) => {
+    if (err) return console.log(err);
+    console.log('File uploaded successfully. ETag:', etag);
+  });
+  
+  minioClient.getObject('cloud', 'myfile.txt', (err, dataStream) => {
+    if (err) return console.log(err);
+    dataStream.pipe(process.stdout);
+  });
