8505d69ff945b6cce02dc29f68de85cd2ab93b30
Smartlinuxcoder <smartcoder@smartlinux.xyz>
2025-01-07 19:01:11 +0100

    I hate myself, drizzle, sveltekit, minio and stupid js

--- src/lib/server/s3/index.js
+++ src/lib/server/s3/index.js
@@ -1,10 +1,10 @@
 import * as Minio from 'minio'
 import 'dotenv/config'
 
-export const minioClient = new Minio.Client({
-    endPoint: process.env.MINIO_HOSTNAME,
-    port: parseInt(process.env.MINIO_PORT),
+export const minioClient = new Minio.Client({  
+    endPoint: process.env.MINIO_HOSTNAME || 'minio',
+    port: parseInt(process.env.MINIO_PORT || '9000'),
     useSSL: process.env.MINIO_SSL === 'true',
-    accessKey: process.env.MINIO_ACCESSKEY,
-    secretKey: process.env.MINIO_SECRETKEY
-});
+    accessKey: process.env.MINIO_ACCESSKEY || 'minioadmin',
+    secretKey: process.env.MINIO_SECRETKEY || 'minioadmin'
+  });
